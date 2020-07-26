bbef24993e9414cc8cc70247f1c36c3b2ed4109d
Dumasjr <aledumasjr@gmail.com>
2020-07-26 00:43:44 -0300

    Criação da tela cadastro criar novo registro

---
+++ app/Http/Controllers/ClientsController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Clients;
+
+
+class ClientsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $clients = Clients::all();
+        return view('clients.grid', compact('clients'));
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+{
+    return view('clients.form');
+}
+
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+
+    public function store(Request $request)
+    {
+        $client= Clients::create($request->all());
+
+        if($client) {
+            return redirect()->route('clients.index');
+        }
+    }
+
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+
+    public function edit($id)
+    {
+        $client = Clients::findOrFail($id);
+
+        if ($client) {
+            return view('clients.form', compact('client'));
+        } else {
+            return redirect()->back();
+        }
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ database/migrations/2020_07_25_231605_create_imoveis_table.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateImoveisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 100);
+            $table->integer('rg')->nullable();
+            $table->integer('cpf')->nullable();
+            $table->string('phone',25);
+            $table->string('cel',25);
+            $table->string('email')->unique();
+            $table->date('birth')->nullable();
+            $table->string('gender',1);
+            $table->text('description')->nullable();
+            $table->double('income',10,2)->nullable();
+            $table->timestamps();
+       });
+       Schema::create('properties', function (Blueprint $table) {
+           $table->increments('id');
+           $table->string('type', 45);
+           $table->string('address',85);
+           $table->string('burgh',55);
+           $table->string('city',55);
+           $table->string('state',2);
+           $table->double('rating',10,2);
+           $table->timestamps();
+      });
+      Schema::create('Details', function (Blueprint $table) {
+       $table->increments('id');
+       $table->integer('area')->nullable();
+       $table->integer('suite')->nullable();
+       $table->integer('dormitory')->nullable();
+       $table->integer('bathroom')->nullable();
+       $table->text('description')->nullable();
+       $table->timestamps();
+   });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('imoveis');
+    }
+}
